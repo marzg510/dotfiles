feb928642a9612f2654182e75427830fb3458dff
masaru gotou <masaru.gotou@gmail.com>
2017-02-26 02:43:32 +0900

    add neosnippet

---
+++ mypack.sh
@@ -0,0 +1,6 @@
+cd ~/.vim/pack/mypack/start/
+#git clone git://github.com/tpope/vim-endwise.git
+git clone https://github.com/Shougo/neosnippet.vim.git
+git clone https://github.com/Shougo/neosnippet-snippets.git
+
+
