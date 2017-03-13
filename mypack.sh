b3e2dd36487888fe3bc408cc288b7063989170f4
masaru.gotou <masaru.gotou@scsk.jp>
2017-03-13 14:49:15 +0900

    add QfixHowm

--- mypack.sh
+++ mypack.sh
@@ -22,3 +22,7 @@ git clone git://github.com/tpope/vim-bundler.git
 #vim-fugitive
 git clone git://github.com/tpope/vim-fugitive.git
 vim -u NONE -c "helptags vim-fugitive/doc" -c q
+
+# QFixHowm
+https://github.com/fuenor/qfixhowm.git
+
