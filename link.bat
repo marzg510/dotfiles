efaffd1e70984fc6fac5391a8c150b3071bf0f50
Masaru Gotou <masaru.gotou@gmail.com>
2017-05-09 23:26:33 +0900

    add teraterm setting

---
+++ link.bat
@@ -0,0 +1,12 @@
+
+:: for win
+::cd vim
+mklink .vimrc ..\dotfiles\vimrc
+mklink .gvimrc ..\dotfiles\gvimrc
+mklink /D C:\Users\hoge\memo D:\gotowork\howm
+mklink /D memo E:\gotowork\Documents\memo
+mklink /D howm E:\gotowork\Documents\memo
+
+:: TeraTerm
+mklink D:\gotowork\tools\teraterm\TERATERM.INI D:\gotowork\tools\dotfiles\TERATERM.INI
+
--- link.sh
+++ link.sh
@@ -3,10 +3,3 @@ ln -s ~/dotfiles/gvimrc ~/.gvimrc
 ln -s ~/dotfiles/irbrc ~/.irbrc
 ln -s ~/dotfiles/bash_profile ~/.bash_profile
 
-# for win
-#cd vim
-#mklink .vimrc ..\dotfiles\vimrc
-#mklink .gvimrc ..\dotfiles\gvimrc
-#mklink /D C:\Users\hoge\memo D:\gotowork\howm
-#mklink /D memo E:\gotowork\Documents\memo
-#mklink /D howm E:\gotowork\Documents\memo
