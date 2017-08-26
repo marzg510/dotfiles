eaad9f4f386f06c909ae3f432287fe6f2a663d6c
Masaru Gotou <masaru.gotou@gmail.com>
2017-08-27 01:04:09 +0900

    add vscode setting,delete bash_profile,add bashrc

--- link.sh
+++ link.sh
@@ -1,7 +1,7 @@
 ln -s ~/dotfiles/vimrc ~/.vimrc
 ln -s ~/dotfiles/gvimrc ~/.gvimrc
 ln -s ~/dotfiles/irbrc ~/.irbrc
-ln -s ~/dotfiles/bash_profile ~/.bash_profile
+ln -s ~/dotfiles/bashrc ~/.bashrc
 mkdir -p ~/.ssh
 ln -s ~/dotfiles/ssh/config ~/.ssh/config
 
@@ -9,4 +9,7 @@ ln -s ~/dotfiles/conkyrc ~/.conkyrc
 
 mkdir -p ~/.vnc
 ln -s ~/dotfiles/vnc/xstartup ~/.vnc/xstartup
+
+mkdir -p ~/.config/Code/User
+ln -s ~/dotfiles/Code/settings.json ~/.config/Code/User/settings.json
 
