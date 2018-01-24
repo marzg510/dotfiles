de1eb5d91f76615507d3053fb1ed34dee1ea1be6
Masaru Gotou <masaru.gotou@gmail.com>
2018-01-24 22:57:44 +0900

    modify git-all-status.sh

--- bin/git-all-status.sh
+++ bin/git-all-status.sh
@@ -1,10 +1,12 @@
 #!/bin/bash
 
 #TARGET_DIRS=(~/workspace/ ~/memo/ ~/dotfiles/)
-TARGET_DIRS=(~/)
+#TARGET_DIRS=(~/)
+#EXCLUDES=(tf-gogh)
 
 #for d in $(find ${TARGET_DIRS[@]} -name .git -type d); do
 for d in $(find ~/*/ -name .git -type d); do
+  [[ $d =~ tf-gogh ]] && continue
   cd $d/..
   [ $(git status | wc -l) -ne 3 ] && pwd
 done
