dc65fc5d0b0127689040994e6564edf212416d87
Masaru Gotou <masaru.gotou@gmail.com>
2020-05-12 08:55:56 +0900

    fix new git status messages

--- bin/git-all-status.sh
+++ bin/git-all-status.sh
@@ -5,9 +5,18 @@
 #EXCLUDES=(tf-gogh)
 
 #for d in $(find ${TARGET_DIRS[@]} -name .git -type d); do
-for d in $(find ~/*/ -name .git -type d); do
+#for d in $(find ~/*/ -name .git -type d); do
+for d in $(find ./*/ -name .git -type d); do
   [[ $d =~ tf-gogh ]] && continue
+  pushd . >/dev/null
+#  echo $d
   cd $d/..
-  [ $(git status | wc -l) -ne 3 ] && pwd
+  if [ $(git status | grep "tree clean$" | wc -l) -ne 1 ]; then
+    echo "*****************"
+    pwd
+    git status
+  fi
+#  [ $(git status | wc -l) -ne 3 ] && pwd
+  popd >/dev/null
 done
 
