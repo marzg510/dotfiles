28b54d57509f6fae6b3d1443c99404512f551afa
Masaru Gotou <masaru.gotou@gmail.com>
2022-11-15 07:10:20 +0000

    stable download

--- bin/install_chrome_driver.sh
+++ bin/install_chrome_driver.sh
@@ -1,10 +1,15 @@
-#!/bin/bash
+#!/bin/bash +x
 # chrome driver
 
 pushd /tmp
 sudo apt-get install -y unzip
 version=$(curl -s "https://chromedriver.storage.googleapis.com/LATEST_RELEASE")
 echo $version
-wget -qP ./ "https://chromedriver.storage.googleapis.com/${version}/chromedriver_linux64.zip"
-sudo unzip -o chromedriver_linux64.zip -d /usr/local/bin
+filename="chromedriver_linux64.zip"
+url="https://chromedriver.storage.googleapis.com/${version}/$filename"
+echo $url
+[[ -f $filename ]]; rm -f $filename
+wget -P ./ $url
+sudo unzip -o $filename -d /usr/local/bin
+chromedriver -version
 popd
