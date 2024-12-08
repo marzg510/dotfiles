8c5eb77916ae8c14f860c897426dc1029acf0687
masaru gotou <masaru.gotou@gmail.com>
2024-12-08 12:40:41 +0900

    modify link.sh

--- link.sh
+++ link.sh
@@ -1,5 +1,5 @@
-# ln -sf ~/dotfiles/vimrc ~/.vimrc
-# ln -sf ~/dotfiles/gvimrc ~/.gvimrc
+ln -sf ~/dotfiles/vimrc ~/.vimrc
+ln -sf ~/dotfiles/gvimrc ~/.gvimrc
 ln -sf ~/dotfiles/irbrc ~/.irbrc
 ln -sf ~/dotfiles/gemrc ~/.gemrc
 ln -sf ~/dotfiles/bashrc ~/.bashrc
@@ -7,13 +7,4 @@ mkdir -p ~/.ssh
 ln -sf ~/dotfiles/ssh/config ~/.ssh/config
 
 ln -sf ~/dotfiles/conkyrc ~/.conkyrc
-
-#mkdir -p ~/.vnc
-#ln -sf ~/dotfiles/vnc/xstartup ~/.vnc/xstartup
-
-#mkdir -p ~/.config/Code/User
-#ln -sf ~/dotfiles/Code/settings.json ~/.config/Code/User/settings.json
-
-# link tools
-#ln -sf ~/dotfiles/bin/git-all-status.sh ~/bin/.
 
